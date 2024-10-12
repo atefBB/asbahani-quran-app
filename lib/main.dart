b58bf8ae2296df1ac7759456431e780582e809d6
Atef Ben Ali <atef.bettaib@gmail.com>
2024-10-12 23:51:06 +0100

    feat: add/use `wakelock_plus` package

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:wakelock_plus/wakelock_plus.dart';
 
 import 'providers/chapter_provider.dart';
 import 'screens/quran_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  WakelockPlus.enable();
 
-//  SystemChrome.setEnabledSystemUIMode(
-  //     SystemUiMode.immersiveSticky); // Enables full-screen mode
   runApp(MultiProvider(providers: [
     ChangeNotifierProvider(create: (_) => ChapterProvider()),
   ], child: const QuranScreen()));
