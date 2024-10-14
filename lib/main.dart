21278f98855edc59d739abef17bd947e792970c6
Atef Ben Ali <atef.bettaib@gmail.com>
2024-10-14 21:00:34 +0100

    refactor: remove `using provider` logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:wakelock_plus/wakelock_plus.dart';
 
-import 'providers/chapter_provider.dart';
 import 'screens/quran_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   WakelockPlus.enable();
 
-  runApp(MultiProvider(providers: [
-    ChangeNotifierProvider(create: (_) => ChapterProvider()),
-  ], child: const QuranScreen()));
+  runApp(const QuranScreen());
 }
--- lib/screens/quran_page.dart
+++ lib/screens/quran_page.dart
@@ -5,10 +5,8 @@ import 'package:dartarabic/dartarabic.dart';
 import 'dart:convert';
 import 'package:flutter/services.dart'; // For loading the JSON file
 import 'dart:async';
-import 'package:provider/provider.dart';
 import 'package:al_quran/al_quran.dart';
 
-import '../providers/chapter_provider.dart';
 import '../data/page_data.dart';
 
 class QuranPage extends StatefulWidget {
@@ -20,20 +18,20 @@ class QuranPage extends StatefulWidget {
 
 class _QuranPageState extends State<QuranPage> {
   TextEditingController searchController = TextEditingController();
-  static PageController pageController = PageController();
+  late PageController _pageController;
+  int _initialPage = 0;
 
   List<dynamic> quran = [];
-  List searchResults = [];
+  List chapters = [];
 
   @override
   void initState() {
-    initialization();
-
     super.initState();
+    initialization();
   }
 
   // Load Quran JSON data from assets
-  Future<void> loadQuranData() async {
+  Future<void> _loadQuranData() async {
     final String response = await rootBundle.loadString('assets/quran.json');
     final List<dynamic> data = await jsonDecode(response);
     setState(() {
@@ -41,21 +39,34 @@ class _QuranPageState extends State<QuranPage> {
     });
   }
 
-  // load the last opened page from SharedPreferences
-  Future<void> _loadLastOpenedPage() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    int lastOpenedPage =
-        prefs.getInt('lastOpenedPage') ?? 1; // Default to page 1
+  Future<void> _loadQuranChapters() async {
+    final String response = await rootBundle.loadString('assets/chapters.json');
+    final List<dynamic> rawData = await jsonDecode(response);
+    setState(() {
+      chapters = rawData.toList();
+    });
+  }
 
-    // Wait until the first frame is rendered before jumping to the page
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      if (pageController.hasClients) {
-        pageController.jumpToPage(lastOpenedPage - 1);
+  Future<void> _loadLastOpenedPage() async {
+    int lastOpenedPage = await _getLastOpenedPage();
+
+    setState(() {
+      if (_pageController.hasClients) {
+        _pageController.jumpToPage(lastOpenedPage - 1);
+      } else {
+        _pageController = PageController(
+            initialPage:
+                lastOpenedPage - 1); // Page starts from 0, so subtract 1
       }
     });
   }
 
-  // Function to save the last opened page in SharedPreferences
+  Future<int> _getLastOpenedPage() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getInt('lastOpenedPage') ?? 1; // Default to page 1
+  }
+
+  // save the last opened page in SharedPreferences
   Future<void> _saveLastOpenedPage(int pageNumber) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     await prefs.setInt('lastOpenedPage', pageNumber);
@@ -77,12 +88,18 @@ class _QuranPageState extends State<QuranPage> {
     }
   }
 
-  void initialization() async {
+  void initialization() {
     // This is where you can initialize the resources needed by your app while
     // the splash screen is displayed.
+    _pageController = PageController(initialPage: _initialPage);
 
-    _loadLastOpenedPage();
-    loadQuranData();
+    _loadQuranData();
+    _loadQuranChapters();
+
+    // Load the last opened page after the first frame
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _loadLastOpenedPage();
+    });
     FlutterNativeSplash.remove();
   }
 
@@ -151,55 +168,43 @@ class _QuranPageState extends State<QuranPage> {
   @override
   Widget build(BuildContext context) {
     const totalPagesNumber = 604;
-    return FutureBuilder(
-        future:
-            Provider.of<ChapterProvider>(context, listen: false).loadChapters(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
 
-          return Consumer<ChapterProvider>(
-            builder: (context, chapterProvider, child) {
-              return GestureDetector(
-                onTap: () => _showMenu(context, chapterProvider.chapters),
-                child: PageView.builder(
-                  controller: pageController,
-                  onPageChanged: (index) {
-                    _saveLastOpenedPage(index + 1);
-                  },
-                  reverse: true, // For RTL navigation
-                  itemCount: totalPagesNumber,
-                  itemBuilder: (context, index) {
-                    return SafeArea(
-                      top: true,
-                      child: Container(
-                        width: MediaQuery.sizeOf(context).width,
-                        height: MediaQuery.sizeOf(context).height,
-                        decoration: const BoxDecoration(
-                          color: Colors.white,
-                        ),
-                        alignment: const AlignmentDirectional(0, 0),
-                        child: Column(
-                          mainAxisSize: MainAxisSize.max,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            _headerRow(index),
-                            _pageImageExpandedRow(context, index + 1),
-                            _footerRow(index),
-                          ],
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              );
-            },
+    return GestureDetector(
+      onTap: () => _showMenu(context),
+      child: PageView.builder(
+        controller: _pageController,
+        onPageChanged: (index) {
+          _saveLastOpenedPage(index + 1);
+        },
+        reverse: true, // For RTL navigation
+        itemCount: totalPagesNumber,
+        itemBuilder: (context, index) {
+          return SafeArea(
+            top: true,
+            child: Container(
+              width: MediaQuery.sizeOf(context).width,
+              height: MediaQuery.sizeOf(context).height,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+              ),
+              alignment: const AlignmentDirectional(0, 0),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  _headerRow(index),
+                  _pageImageExpandedRow(context, index + 1),
+                  _footerRow(index),
+                ],
+              ),
+            ),
           );
-        });
+        },
+      ),
+    );
   }
 
-  void _showMenu(BuildContext context, chapters) {
+  void _showMenu(context) {
     // @todo add bookmarked pages as tab
 
     showModalBottomSheet(
@@ -222,7 +227,7 @@ class _QuranPageState extends State<QuranPage> {
                 Expanded(
                   child: TabBarView(
                     children: [
-                      _chapterTab(context, chapters),
+                      _chapterTab(context),
                       _searchTab(context),
                     ],
                   ),
@@ -235,37 +240,30 @@ class _QuranPageState extends State<QuranPage> {
     );
   }
 
-  Widget _chapterTab(context, chapters) {
+  Widget _chapterTab(context) {
     return ListView.builder(
       itemCount: chapters.length,
       itemBuilder: (BuildContext context, int index) {
-        var chapterPageNumber = chapters[index].pageNumber;
+        var chapterPageNumber = chapters[index]["page"];
+        var chapterName = DartArabic.stripTashkeel(chapters[index]["name_ar"]);
 
         return ListTile(
-          title: Text(
-              DartArabic.stripTashkeel(chapters[index].name)), // Chapter name
-          subtitle: Text('صفحة  $chapterPageNumber'), // Chapter page number
+          title: Text(chapterName),
+          subtitle: Text('صفحة  $chapterPageNumber'),
           onTap: () {
             Navigator.pop(context);
-            pageController.jumpToPage(chapterPageNumber - 1);
+            if (_pageController.hasClients) {
+              _pageController.jumpToPage(chapterPageNumber - 1);
+            }
           },
         );
       },
     );
   }
 
-  // Perform search by filtering ayahs
-  void _performSearch(String query) {
-    setState(() {
-      searchResults = quran.where((ayah) {
-        return ayah['aya_text_emlaey']
-            .toLowerCase()
-            .contains(query.toLowerCase());
-      }).toList();
-    });
-  }
-
   Widget _searchTab(BuildContext context) {
+    List searchResults = [];
+
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Column(
@@ -277,18 +275,25 @@ class _QuranPageState extends State<QuranPage> {
               border: OutlineInputBorder(),
             ),
             onChanged: (String query) {
-              _performSearch(query);
+              setState(() {
+                searchResults = quran.where((ayah) {
+                  return ayah['aya_text_emlaey']
+                      .toLowerCase()
+                      .contains(query.toLowerCase());
+                }).toList();
+                debugPrint("search: $searchResults");
+              });
             },
           ),
           Expanded(
-            child: _newSearchResultsWidget(context, searchResults),
+            child: _searchResultsWidget(context, searchResults),
           ),
         ],
       ),
     );
   }
 
-  Widget _newSearchResultsWidget(context, searchResults) {
+  Widget _searchResultsWidget(context, searchResults) {
     if (searchResults.isEmpty) {
       return const Center(child: Text('لم يتم العثور على نتائج'));
     }
@@ -313,7 +318,9 @@ class _QuranPageState extends State<QuranPage> {
               contentPadding: const EdgeInsets.symmetric(horizontal: 16),
               onTap: () {
                 Navigator.of(context).pop();
-                pageController.jumpToPage(ayah["page"] - 1);
+                if (_pageController.hasClients) {
+                  _pageController.jumpToPage(ayah["page"] - 1);
+                }
               },
             ),
             const Divider(
